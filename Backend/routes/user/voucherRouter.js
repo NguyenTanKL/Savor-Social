014e1dcf885bb2d94babc61cf45dc717af2d94c8
dangnguyennhat <129052433+dangnguyennhat@users.noreply.github.com>
2025-03-26 20:13:06 +0700

    cloudinary

--- Backend/routes/user/voucherRouter.js
+++ Backend/routes/user/voucherRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const upload = require('../../controllers/helper/UploadVoucher')
+const { upload } = require("../../config/cloudinary/cloudinaryConfig");
 
 const voucherController = require('../../controllers/user/VoucherController');
 
