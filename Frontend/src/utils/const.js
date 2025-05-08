dd21fbd44ac9b43b93198a75fc7f6a9a26cbee77
HuyWithNY <huyhmopubg@gmail.com>
2025-05-08 23:55:30 +0700

    thay doi url

--- Frontend/src/utils/const.js
+++ Frontend/src/utils/const.js
@@ -1,2 +1,4 @@
-export const BACKENDURL = "http://localhost:5000";
-export const FRONTENDURL = "http://localhost:3000";
+// export const BACKENDURL = "http://localhost:5000";
+// export const FRONTENDURL = "http://localhost:3000";
+export const FRONTENDURL = "https://savor-social-1.onrender.com";
+export const BACKENDURL = "https://savor-social-es5s.onrender.com";
