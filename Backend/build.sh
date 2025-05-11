11ff917a49be290c7a080f0c08c0489e468badb5
HuyWithNY <huyhmopubg@gmail.com>
2025-05-11 16:38:35 +0700

    edit build.sh

--- Backend/build.sh
+++ Backend/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+chmod +x build.sh
 # Cài đặt Python và pip
 apt-get update
 apt-get install -y python3 python3-pip
 # Cài đặt các thư viện từ requirements.txt
-pip3 install -r requirements.txtgit 
+pip3 install -r requirements.txt
